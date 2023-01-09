884d7ed6ee0de5ab02a1eb3a433b250f738579cf
huynhan47 <huynhan007@gmail.com>
2023-01-09 22:14:26 +0700

    Update EmployeeController.java
    tiny

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "https://vnpt-backend.herokuapp.com")
+@CrossOrigin(origins = "http://vnpt-backend.herokuapp.com")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
