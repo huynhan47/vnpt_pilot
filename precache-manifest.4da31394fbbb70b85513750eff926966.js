b013e2fabb672e7a006936164b79da359f54b0b6
huynhan47 <huynhan007@gmail.com>
2023-01-10 00:40:12 +0700

    Updates

---
+++ precache-manifest.4da31394fbbb70b85513750eff926966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c085e010f659fd5d88cc1e44965228",
+    "url": "/vnpt_pilot/index.html"
+  },
+  {
+    "revision": "4276bc99dcfb8e1a1f21",
+    "url": "/vnpt_pilot/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "538539bf8283da3ad366",
+    "url": "/vnpt_pilot/static/css/main.e2bc9fc6.chunk.css"
+  },
+  {
+    "revision": "4276bc99dcfb8e1a1f21",
+    "url": "/vnpt_pilot/static/js/2.adf17f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vnpt_pilot/static/js/2.adf17f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538539bf8283da3ad366",
+    "url": "/vnpt_pilot/static/js/main.a6a84439.chunk.js"
+  },
+  {
+    "revision": "27262f5b3121ae2afa13",
+    "url": "/vnpt_pilot/static/js/runtime-main.101b1500.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/vnpt_pilot/static/media/logo.ee7cd8ed.svg"
+  }
+]);
