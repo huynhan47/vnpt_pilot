b4adc3f4aaa46b20231fa6997959a767991d7b1d
unknown <nhk06052001@gmail.com>
2023-01-11 08:37:57 +0700

    New header, menu bar, footer

--- react-frontend/src/components/ListEmployeeComponent.jsx
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,128 +1,150 @@
-import React, { Component } from 'react'
-import EmployeeService from '../services/EmployeeService'
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import { purple } from '@mui/material/colors';
+import React, { Component } from "react";
+import EmployeeService from "../services/EmployeeService";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 
 const BootstrapButton = styled(Button)({
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 16,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    backgroundColor: '#0063cc',
-    borderColor: '#0063cc',
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-      backgroundColor: '#0069d9',
-      borderColor: '#0062cc',
-      boxShadow: 'none',
-    },
-    '&:active': {
-      boxShadow: 'none',
-      backgroundColor: '#0062cc',
-      borderColor: '#005cbf',
-    },
-    '&:focus': {
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-    },
-  });
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 16,
+  padding: "6px 12px",
+  border: "1px solid",
+  lineHeight: 1.5,
+  backgroundColor: "#0063cc",
+  borderColor: "#0063cc",
+  fontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    '"Segoe UI"',
+    "Roboto",
+    '"Helvetica Neue"',
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(","),
+  "&:hover": {
+    backgroundColor: "#0069d9",
+    borderColor: "#0062cc",
+    boxShadow: "none",
+  },
+  "&:active": {
+    boxShadow: "none",
+    backgroundColor: "#0062cc",
+    borderColor: "#005cbf",
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+});
 
 const ColorButton = styled(Button)({
-    marginLeft: '10px'
-  });
+  marginLeft: "10px",
+});
 
 class ListEmployeeComponent extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            employees: []
-        }
-        this.addEmployee = this.addEmployee.bind(this);
-        this.editEmployee = this.editEmployee.bind(this);
-        this.deleteEmployee = this.deleteEmployee.bind(this);
-    }
+    this.state = {
+      employees: [],
+    };
+    this.addEmployee = this.addEmployee.bind(this);
+    this.editEmployee = this.editEmployee.bind(this);
+    this.deleteEmployee = this.deleteEmployee.bind(this);
+  }
 
-    deleteEmployee(id) {
-        EmployeeService.deleteEmployee(id).then(res => {
-            this.setState({ employees: this.state.employees.filter(employee => employee.id !== id) });
-        });
-    }
-    viewEmployee(id) {
-        this.props.history.push(`/view-employee/${id}`);
-    }
-    editEmployee(id) {
-        this.props.history.push(`/add-employee/${id}`);
-    }
+  deleteEmployee(id) {
+    EmployeeService.deleteEmployee(id).then((res) => {
+      this.setState({
+        employees: this.state.employees.filter(
+          (employee) => employee.id !== id
+        ),
+      });
+    });
+  }
+  viewEmployee(id) {
+    this.props.history.push(`/view-employee/${id}`);
+  }
+  editEmployee(id) {
+    this.props.history.push(`/add-employee/${id}`);
+  }
 
-    componentDidMount() {
-        EmployeeService.getEmployees().then((res) => {
-            this.setState({ employees: res.data });
-        });
-    }
+  componentDidMount() {
+    EmployeeService.getEmployees().then((res) => {
+      this.setState({ employees: res.data });
+    });
+  }
 
-    addEmployee() {
-        this.props.history.push('/add-employee/_add');
-    }
+  addEmployee() {
+    this.props.history.push("/add-employee/_add");
+  }
 
-    render() {
-        return (
-            <div>
-                <h2 className="text-center">Employees List</h2>
-                <div className="row">
-                    <BootstrapButton variant="contained" disableRipple onClick={this.addEmployee}> Add Employee</BootstrapButton>
-                </div>
-                <br></br>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-
-                        <thead>
-                            <tr>
-                                <th> Employee First Name</th>
-                                <th> Employee Last Name</th>
-                                <th> Employee Email Id</th>
-                                <th> Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.employees.map(
-                                    employee =>
-                                        <tr key={employee.id}>
-                                            <td> {employee.firstName} </td>
-                                            <td> {employee.lastName}</td>
-                                            <td> {employee.emailId}</td>
-                                            <td>
-                                                <ColorButton variant="contained" color="success" size="small"  onClick={() => this.editEmployee(employee.id)}>Update</ColorButton>
-                                                <ColorButton variant="outlined" color="error" size="small" onClick={() => this.deleteEmployee(employee.id)} >
-                                                    Delete
-                                                </ColorButton>
-                                                <ColorButton variant="outlined" size="small" onClick={() => this.viewEmployee(employee.id)}>View</ColorButton>                                               
-                                            </td>
-                                        </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-
-                </div>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">Employees List</h2>
+        <div className="row">
+          <BootstrapButton
+            variant="contained"
+            disableRipple
+            onClick={this.addEmployee}
+          >
+            {" "}
+            Add Employee
+          </BootstrapButton>
+        </div>
+        <br></br>
+        <div className="row">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th> Employee First Name</th>
+                <th> Employee Last Name</th>
+                <th> Employee Email Id</th>
+                <th> Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.employees.map((employee) => (
+                <tr key={employee.id}>
+                  <td> {employee.firstName} </td>
+                  <td> {employee.lastName}</td>
+                  <td> {employee.emailId}</td>
+                  <td>
+                    <ColorButton
+                      variant="contained"
+                      color="success"
+                      size="small"
+                      onClick={() => this.editEmployee(employee.id)}
+                    >
+                      Update
+                    </ColorButton>
+                    <ColorButton
+                      variant="outlined"
+                      color="error"
+                      size="small"
+                      onClick={() => this.deleteEmployee(employee.id)}
+                    >
+                      Delete
+                    </ColorButton>
+                    <ColorButton
+                      variant="outlined"
+                      size="small"
+                      onClick={() => this.viewEmployee(employee.id)}
+                    >
+                      View
+                    </ColorButton>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ListEmployeeComponent
+export default ListEmployeeComponent;
